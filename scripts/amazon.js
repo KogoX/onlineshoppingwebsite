41fff2f78a33a84b520f143d7253fcc0c0d411cd
KogoX <kogodennis390@gmail.com>
2025-04-07 09:02:49 -0700

    Use projucts.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,6 @@
-const product = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Athletic Cotton Socks (6 Pairs)',
-  rating: {
-    stars: 4.5,
-    reviews: 87
-  },
-  priceCents: 1090,
-},{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball)',
-  rating: {
-    stars: 4,
-    reviews: 127
-  },
-  priceCents: 2095
-},{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    reviews: 100
-  },
-  priceCents: 9995
-}];
-
 let productsHTML = ''
 
-product.forEach((product) => {
+products.forEach((product) => {
   productsHTML += `
   <div class="product-container">
           <div class="product-image-container">
@@ -42,7 +16,7 @@ product.forEach((product) => {
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.reviews}
+              ${product.rating.count}
             </div>
           </div>
 
